8a608ae2ccb06a0c94fd24832381ddcdb02127c7
Kseniia Pidopryhora <ksenia.pidopryhora@gmail.com>
2023-02-20 17:50:29 +0200

    add sanity studio

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1 className="text-4xl ">Welcome to the BLog</h1>
+      <p>content</p>
+    </div>
+  );
+};
+
+export default HomePage;
