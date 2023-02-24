4c154cab15b43574a8a5c9b71a743243f49bf1a6
Kseniia Pidopryhora <ksenia.pidopryhora@gmail.com>
2023-02-24 23:08:02 +0200

    add revalidation for every 60 seconds

--- app/(user)/post/[slug]/page.tsx
+++ app/(user)/post/[slug]/page.tsx
@@ -12,6 +12,23 @@ type Props = {
   };
 };
 
+export const revalidate = 60; //revalidates this page every 60 seconds
+
+export async function generateStaticParams() {
+  const query = groq`
+  *[_type=='post']
+  {
+    slug
+  }`;
+
+  const slugs: Post[] = await client.fetch(query);
+  const slugRoutes = slugs.map((slug) => slug.slug.current);
+
+  return slugRoutes.map((slug) => ({
+    slug,
+  }));
+}
+
 async function Post({ params: { slug } }: Props) {
   const query = groq`
   *[_type=='post' && slug.current == $slug][0] 
@@ -83,7 +100,7 @@ async function Post({ params: { slug } }: Props) {
         </div>
       </section>
 
-      <PortableText value={post.body} components={RichTextComponents}/>
+      <PortableText value={post.body} components={RichTextComponents} />
     </article>
   );
 }
