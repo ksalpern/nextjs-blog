979903e46448f51be56b7fa3c842c106220c0927
Kseniia Pidopryhora <ksenia.pidopryhora@gmail.com>
2023-02-24 20:42:01 +0200

    add studio preview mode

--- sanity.config.ts
+++ sanity.config.ts
@@ -3,6 +3,7 @@ import { deskTool } from 'sanity/desk'
 import { visionTool } from '@sanity/vision'
 import { schemaTypes } from './schemas'
 import { myTheme } from './theme'
+import { getDefaultDocumentNode } from './structure'
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!
@@ -13,9 +14,14 @@ export default defineConfig({
   title: 'next_sanity_blog',
   projectId,
   dataset,
-  plugins: [deskTool(), visionTool()],
+  plugins: [
+    deskTool({
+      defaultDocumentNode: getDefaultDocumentNode
+    }),
+    visionTool()
+  ],
   schema: {
     types: schemaTypes
   },
-  theme:myTheme
+  theme: myTheme
 })
